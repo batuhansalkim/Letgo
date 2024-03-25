33d501ffe793e1d09f7e52dd9ffcf0b7110bae11
batuhan <batusalkim11@gmail.com>
2024-03-25 03:13:36 +0300

    filtering page filters continue

--- src/components/FilteringCategory/index.tsx
+++ src/components/FilteringCategory/index.tsx
@@ -1,7 +1,11 @@
 import React,{useState,useEffect} from "react";
-import { View,Text,ScrollView,Image } from "react-native";
+import { View,Text,ScrollView,Image,StyleSheet,Dimensions } from "react-native";
 import filteringsData from "../../../assets/filterings";
 import { Filtering } from "../../models";
+import {Feather} from "@expo/vector-icons";
+
+
+const {width,height} = Dimensions.get("window");
 
 export default function index(){
     const [filtering,setFiltering] = useState<Filtering[]>([]);
@@ -13,8 +17,28 @@ export default function index(){
         }
     },[])
     return(
-        <ScrollView>
-            
+        <ScrollView style={{marginHorizontal:12,marginVertical:14}} horizontal={true} bounces={true} showsHorizontalScrollIndicator={false}>
+            {filtering.map((item:Filtering)=>(
+                <View key={item.id} style={[{backgroundColor:"#f5f5f5"},styles.filteringBadge]}>
+                    <Text>{item.name}</Text>
+                    <Feather style={{}} name="chevron-down" size={20}/>
+                </View>
+            ))}
         </ScrollView>
     )
 }
+
+
+const styles = StyleSheet.create({
+    filteringBadge:{
+        flexDirection:"row",
+        paddingHorizontal:6,
+        // backgroundColor:"red",
+        height:height*0.04,
+        marginRight:7,
+        alignItems:"center",
+        borderRadius:20,
+        justifyContent:"center",
+        
+    }
+})
