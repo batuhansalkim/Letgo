08fcd6ff33035b93461a8fb15d023b03eafdc85f
batuhan <batusalkim11@gmail.com>
2024-03-29 07:24:54 +0300

    map component continue 07:24

--- src/components/MappingComponent/index.tsx
+++ src/components/MappingComponent/index.tsx
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text,Dimensions } from 'react-native'
+import React from 'react';
+import MapView,{PROVIDER_GOOGLE} from "react-native-maps";
+
+const {width,height} = Dimensions.get("window");
 
 const index = () => {
   return (
     <View>
-      <Text>index</Text>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+            latitude:41.0082,
+            longitude:28.9784,
+            latitudeDelta:0,
+            longitudeDelta:0.5,
+        }}
+        style={{height:height*0.18,borderRadius:12,marginTop:18}}
+      />
     </View>
   )
 }
--- src/screens/ProductDetailsScreen/index.tsx
+++ src/screens/ProductDetailsScreen/index.tsx
@@ -4,6 +4,8 @@ import { Product } from '../../models';
 import ImageCoursel from "../../components/ImageCarousel";
 import DetailsTextBox from "../../components/DetailsTextBox";
 import SellerProfile from "../../components/SellerProfile";
+import MappingComponent from "../../components/MappingComponent";
+
 const index = (props) => {
   const [product,setProduct] = useState<Product>(props.route.params.product);
   return (
@@ -12,6 +14,7 @@ const index = (props) => {
       <View style={{paddingHorizontal:20,paddingVertical:14}}>
         <DetailsTextBox price={product.price} name={product.name} description={product.description}/>
         <SellerProfile averageRating={product.rating}/>
+        <MappingComponent/>
       </View>
       
     </ScrollView>
