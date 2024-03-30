6d636c6e65b3f148a83fb481a2eced7709ebac62
batuhan <batusalkim11@gmail.com>
2024-03-30 06:37:21 +0300

    map this is a big problem

--- src/components/MappingComponent/index.tsx
+++ src/components/MappingComponent/index.tsx
@@ -15,8 +15,11 @@ const index = () => {
             latitudeDelta:0,
             longitudeDelta:0.5,
         }}
-        style={{height:height*0.18,borderRadius:12,marginTop:18}}
+        style={{height:height*0.19,marginTop:18,borderBottomLeftRadius:12,borderBottomRightRadius:12}}
       />
+      <View style={{height:height*0.044,marginTop:-height*0.04,borderBottomWidth:0.2,borderColor:"#a6a6a6",borderRightWidth:0.2,borderBottomLeftRadius:12,borderBottomRightRadius:12,backgroundColor:"white"}}>
+
+      </View>
     </View>
   )
 }
