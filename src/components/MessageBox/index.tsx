0af01e9ec3e6a2fbdd63e3bef99590bc13d599ae
batuhan <batusalkim11@gmail.com>
2024-03-30 22:32:26 +0300

    Message Box is being made

--- src/components/MessageBox/index.tsx
+++ src/components/MessageBox/index.tsx
@@ -4,13 +4,22 @@ import React,{useState} from 'react'
 
 const {height,width} = Dimensions.get("window");
 
-
+const MessageBox(){
+  
+}
 const index = () => {
+
+  const [searchValue,setSearchValue ] = useState<string>("Hala müsait mi ?");
+  const [messages,setMessages] = useState<string[]>(["Hala satılık mı ?","Fiyata pazarlık payı var mı ?","Ne durumda ?"]);
+
+
   return (
     <View style={{position:"absolute",bottom:0,width:"100%",height:height*0.15,backgroundColor:"white",shadowColor:"gray",shadowOpacity:0.3,
     }}>
-      <ScrollView>
-
+      <ScrollView horizontal={true} bounces={true} showsHorizontalScrollIndicator={false}>
+        {messages.map((item,index)=>(
+          <Text key={index}>{item}</Text>
+        ))}
       </ScrollView>
 
       <View>
